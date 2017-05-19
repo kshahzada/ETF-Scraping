28b9331676167b739b3be5b69480833cad8ee479
Kaspar Shahzada <kshahza@uwo.ca>
2017-05-19 00:01:26 -0400

    Task Manager Initial Implementation

--- TaskManager.py
+++ TaskManager.py
@@ -1,2 +1,32 @@
 import ETFScraper
 import StockScraper
+import datetime
+
+stockUpdateTimer = datetime.datetime.now()
+stockUpdateDelay = 60 #seconds
+ETFUpdateTimer = datetime.datetime.now()
+ETFUpdateDelay = 1 #days
+heartBeatTimer = datetime.datetime.now()
+heartBeatDelay = 20 #seconds
+
+
+while(True):
+    now = datetime.datetime.now()
+    if((stockUpdateDelay <= (now - stockUpdateTimer).seconds) and (now.hour > 4) and (now.hour < 18)):
+        StockScraper.takeSnapShot()
+        stockUpdateTimer = now
+        print("Stock Snapshot Taken!")
+
+    if((ETFUpdateDelay <= (now - ETFUpdateTimer).days)):
+        ETFScraper.updateETFHoldings()
+        StockScraper.addTickers(ETFScraper.getAllStockTickers()['Ticker'])
+        ETFUpdateTimer = now
+        print("Updated ETFs and Stocks Directory")
+
+    if(heartBeatDelay <= (now - heartBeatTimer).seconds):
+        heartBeatTimer = now
+        print("Running Stock Recorder!")
+
+
+
+    
